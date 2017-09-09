c2078746173e86e04e9f74053101d59a2c853301
debanikbasu <31296447+debanikbasu@users.noreply.github.com>
2017-09-09 06:46:04 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+##The function makeCacheMatrix creates a special "matrix" object that can cache its inverse
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) {              
+      inv <- NULL
+      set <- function(y) {                                      ## Create set function to store the matrix passed in the call as x and NULL as inv, both in cache
+            x <<- y
+            inv <<- NULL
+      }
+      get <- function() x                                       ## Create function to get/return the matrix passed in the command line call to '$set
+      setinverse <- function(inv_matrix) inv <<- inv_matrix     ## Create function to set the value of inv in cache to the value of inv_matrix passed in the call to '$setinverse
+      getinverse <- function() inv                              ## Create function to retrieve value of inv from cache and return inv to the caller so we can check it for NULL
+      list(set = set, get = get,
+           setinverse = setinverse,
+           getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+##This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+##If the inverse has already been calculated (and the matrix has not changed), then the cacheSolve will retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      inv <- x$getinverse()                                     ## Get the value for inv in the cache environment and put it in a local inv
+      if(!is.null(inv)) {
+            message("getting cached data")                      ## If inv not NULL, return the value of inv with a message
+            return(inv)
+      }
+      my_matrix <- x$get()                                      ## Call the nested function x$get in makeCacheMatrix to obtain the UNinverted matrix with which to start, and assign it to my_matrix
+      inv <- solve(my_matrix, ...)                              ## Use solve() to invert the my_gmatrix.  Assign the result to inv
+      x$setinverse(inv)                                         ## Call nested function x$setinverse() in makeCacheMatrix to set inv in the cache environment to the local non-NULL inverted result in inv
+      inv
 }
